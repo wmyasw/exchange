c2259a6e1c3dc2599cdeb2edff7147ea72c1eab9
wangmingyu <wangmingyu@jdjt.net>
2016-09-08 18:13:56 +0800

    增加 FlowLayout 支持 ，编写动态标签，增加标签适配器

---
+++ app/src/main/java/com/jdjt/exchange/adapter/TagsAdapter.java
@@ -0,0 +1,12 @@
+package com.jdjt.exchange.adapter;
+
+/**
+ * @author wmy
+ * @Description:
+ * @version:${MODULE_VERSION}
+ * @FileName:TagsAdapter
+ * @Package com.jdjt.exchange.adapter
+ * @Date 2016/9/8 17:46
+ */
+public class TagsAdapter {
+}
