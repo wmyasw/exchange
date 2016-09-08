381711fd890c2a10a6671974babdb924c6117f98
wangmingyu <wangmingyu@jdjt.net>
2016-09-08 18:13:56 +0800

    1、增加 FlowLayout 支持 ，编写动态标签，增加标签适配器 2、调整 上拉下拉菜单布局适配 从展示到隐藏为一个整体，

---
+++ app/src/main/java/com/jdjt/exchange/adapter/TagsAdapter.java
@@ -0,0 +1,47 @@
+package com.jdjt.exchange.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.jdjt.exchange.R;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author wmy
+ * @Description:
+ * @version:${MODULE_VERSION}
+ * @FileName:TagsAdapter
+ * @Package com.jdjt.exchange.adapter
+ * @Date 2016/9/8 17:46
+ */
+public class TagsAdapter extends AppBaseAdapter<Map<String,String>,AppBaseAdapter.BaseViewHolder> {
+
+    public TagsAdapter(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected BaseViewHolder createViewHolder(int position, ViewGroup parent) {
+        return new BaseViewHolder(View.inflate(getContext(), R.layout.layout_tags_item,null));
+    }
+
+    @Override
+    protected void bindViewHolder(BaseViewHolder holder, int position,Map<String, String> data) {
+        String map= data.get("title");
+
+        TextView txt = holder.getView(R.id.tags_name);
+        txt.setText(map);
+
+    }
+
+//    @Override
+//    protected void bindViewHolder(BaseViewHolder holder, int position, String data) {
+//        TextView txt = holder.getView(R.id.tags_name);
+//        txt.setText(data+"");
+//    }
+
+}
